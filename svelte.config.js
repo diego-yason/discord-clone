ca752c2bc9063ca4a46603e49c51a93340191d9e
KingMarine <61825575+KingMarine-GH@users.noreply.github.com>
2022-02-11 09:39:16 +0800

    changed formatting rules

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,27 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
-import path from 'path';
+import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess";
+import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: [
+        preprocess({
+            postcss: true,
+        }),
+    ],
 
-	kit: {
-		adapter: adapter(),
-		vite: {
-			resolve: {
-				alias: {
-					$src: path.resolve('./src')
-				}
-			}
-		}
-	}
+    kit: {
+        adapter: adapter(),
+        vite: {
+            resolve: {
+                alias: {
+                    $src: path.resolve("./src"),
+                },
+            },
+        },
+    },
 };
 
 export default config;
